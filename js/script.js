c31727a808e5d4df3bd38716ec864ee03ea514ae
Daniel Springer <dssp100@gmail.com>
2018-04-24 06:00:12 -0400

    fixed the issue with bottom li

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ $(document).ready(() => {
       var newTodo = $(this).val();
       // Add new todo to ul
       $('ul').append("<li><span><i class='fa fa-trash'></i> </span>"+ newTodo +"</li>")
-      
       // Clear inputs field
       $(this).val('')
 
